945906dd0a76837488ecdf92456816144e00df63
robmainah <44176348+robmainah@users.noreply.github.com>
2024-07-20 13:18:40 +0300

    placeholder data using previous data

--- src/pages/WithQuery.jsx
+++ src/pages/WithQuery.jsx
@@ -1,8 +1,9 @@
-import { useQueries, useQuery } from '@tanstack/react-query';
+import { keepPreviousData, useQueries, useQuery } from '@tanstack/react-query';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
-const getPosts = async () => {
-  const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+const getPosts = async (page) => {
+  const response = await fetch(`https://jsonplaceholder.typicode.com/posts?_page=${page}`);
   return response.json();
 }
 
@@ -12,11 +13,19 @@ const getUsers = async () => {
 }
 
 const WithQuery = () => {
-    const [{ isPending, error, data }, { isPending: userPending, error: userError, data: userData }] = useQueries({
-        queries: [
-            { queryKey: ['posts'], queryFn: getPosts, staleTime: 10000 },
-            { queryKey: ['users'], queryFn: getUsers, staleTime: 10000 },
-        ]
+    const [page, setPage] = useState(1)
+    // const [{ isPending, error, data }, { isPending: userPending, error: userError, data: userData }] = useQueries({
+    //     queries: [
+    //         { queryKey: ['posts', page], queryFn: () => getPosts(page), staleTime: 10000 },
+    //         { queryKey: ['users'], queryFn: getUsers, retry: 3 },
+    //     ]
+    // })
+
+    const { isPending, error, data, isFetching } = useQuery({
+        queryKey: ['posts', page],
+        queryFn: () => getPosts(page),
+        staleTime: 10000,
+        placeholderData: keepPreviousData,
     })
 
     if (isPending) {
@@ -31,14 +40,21 @@ const WithQuery = () => {
         <div className='m-4 max-w-[600px] w-4/5 mx-auto'>
             <Link to="/withoutquery"className='bg-gray-300 block w-fit my-8 mx-auto text-center py-2 px-4 hover:bg-gray-20'>Without Query</Link>
             <h1 className='text-3xl text-center my-8 font-bold text-gray-400'>Posts Data</h1>
-            {data && data.map(post => {
-                return (
-                    <Link to={`${post.id}`} key={post.id} className='p-4 block rounded-lg border border-gray-200 my-6 cursor-pointer hover:bg-gray-900'>
-                        <h2 className='font-bold text-lg mb-2 text-gray-400'>{post.title}</h2>
-                        <p className='text-gray-400'>{post.body}</p>
-                    </Link>
-                )
-            })}
+            <div className={`${isFetching ? 'bg-gray-300 opacity-50' : ''}`}>
+                {data && data.map(post => {
+                    return (
+                        <Link to={`${post.id}`} key={post.id} className='p-4 rounded-lg block border border-gray-200 my-6 cursor-pointer hover:bg-gray-900'>
+                            <h2 className='font-bold text-lg mb-2 text-gray-400'>{post.title}</h2>
+                            <p className='text-gray-400'>{post.body}</p>
+                        </Link>
+                    )
+                })}
+            </div>
+            <div className='flex items-center justify-center gap-2'>
+                <button className='px-3 py-1 bg-blue-500 rounded-md text-white font-bold' onClick={() => setPage(prev => prev > 1 ? prev - 1 : 1)}>Prev</button>
+                <p className='text-gray-100'>Current page: {page}</p>
+                <button className='px-3 py-1 bg-blue-500 rounded-md text-white font-bold' onClick={() => setPage(prev => prev + 1)}>Next</button>
+            </div>
         </div>
     );
 };
